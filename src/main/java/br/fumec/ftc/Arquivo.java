ead91d7a4129128b897643facb61a7392bcf642a
ericvyc <eric_vilar2007@hotmail.com>
2016-04-13 11:31:34 -0300

    Main e jar to exe.

--- src/main/java/br/fumec/ftc/Arquivo.java
+++ src/main/java/br/fumec/ftc/Arquivo.java
@@ -1,9 +1,42 @@
 package br.fumec.ftc;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Arquivo extends JFrame {
+	public Arquivo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Auto Instrucional");
+
+		JLabel lbl_arquivo = new JLabel("Busque seu arquivo de AFN:");
+		JFileChooser selectArquivo = new JFileChooser();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(22)
+						.addComponent(lbl_arquivo)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(selectArquivo, GroupLayout.PREFERRED_SIZE, 494, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(49, Short.MAX_VALUE))
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(51)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(selectArquivo, GroupLayout.PREFERRED_SIZE, 306, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lbl_arquivo))
+						.addContainerGap(51, Short.MAX_VALUE))
+				);
+		this.setSize(800, 500);
+		this.setLocationRelativeTo(null);
+		getContentPane().setLayout(groupLayout);
+	}
 
 	private static final long serialVersionUID = 3150213557789221860L;
-
 }
---
+++ src/main/java/br/fumec/ftc/Main.java
@@ -0,0 +1,10 @@
+package br.fumec.ftc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Arquivo arquivo = new Arquivo();
+		arquivo.setVisible(true);
+	}
+
+}
